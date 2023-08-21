bc7720e322963810b0f7cbd7c1555285c4bf5980
Angel Salmeron <shangel2003@gmail.com>
2023-08-21 15:25:22 -0600

    solo para guardar

--- Views/blog.php
+++ Views/blog.php
@@ -1,7 +1,7 @@
 <?php
 include_once("../Controllers/controlblog.php");
 include_once("../Models/index.php");
-
+include_once "layout.php";
 $pblog = new modeloControllerblog();
 $datoblog = $pblog->blogprincipal();
 
@@ -17,54 +17,16 @@ $datoblog = $pblog->blogprincipal();
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <title>BLOG 11.11lamaletadesueño</title>
-    <script src="https://kit.fontawesome.com/41bcea2ae3.js" crossorigin="anonymous"></script>
+<title>Blog</title>
+    <?php MostrarHead(); ?>
     <link rel="stylesheet" href="../Views/Assets/css/estilos.css">
-    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
- 
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Servicios</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet"
-      integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"
-      integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz"
-      crossorigin="anonymous"></script>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" />
-    <link rel="stylesheet" href="../Views/Assets/css/servicios.css" />
-  
 </head>
 <body>
 
-    <!--Header - menu-->
-        <nav>
-            <div class="nav-container">
-              <div class="main-info-container">
-                <img class="logo-img" src="../Views/Assets/imgs/logo.png" alt="" />
-              </div>
-              <div class="data-container">
-                <div class="servicios">
-                  <a class="nav-link Dropdown" aria-current="page" href="../Views/servicios.html">
-                    <h5>SERVICIOS</h5>
-                  </a>
-                </div>
-                <div class="servicios">
-                  <a class="nav-link Dropdown" aria-current="page" href="../Views/home.html">
-                  <h5>PAQUETES DE VIAJE</h5>
-                </a>
-                </div>
-                <div class="servicios">
-                  <h5>BLOG</h5>
-                </div>
-                <div class="servicios">   
-                  <a class="nav-link Dropdown" aria-current="page" href="../Views/Buscador.html">
-                  <h5>BUSCADOR VUELOS</h5>
-                </a>
-                </div>
-              </div>
-            </div>
-          </nav>
+<?php 
+
+        MostrarHeader();
+    ?>
     
 
     <!--Portada-->
@@ -93,28 +55,16 @@ $datoblog = $pblog->blogprincipal();
       <?php endif; ?>
     </div>
 
-    <div class="container-footer">	
 
-        <footer>
-            <div class="logo-footer">
-                <img src="/Assets/imgs/logo.png" alt="">
-            </div>
-
-            <div class="redes-footer">
-                <a href="#"><i class="fab fa-facebook-f icon-redes-footer"></i></a>
-                <a href="#"><i class="fab fa-google-plus-g icon-redes-footer"></i></a>
-                <a href="#"><i class="fab fa-instagram icon-redes-footer"></i></a>
-            </div>
-
-        
-            
-
-        </footer>
-
-    </div>
 
 </div>
 
     <script src="js/script.js"></script>
+    <footer>
+    <?php
+    MostrarFooter();
+
+    ?>
+      </footer>
 </body>
 </html>
--- Views/post.php
+++ Views/post.php
@@ -1,6 +1,7 @@
 <?php
 include_once("../Controllers/controlblog.php");
 include_once("../Models/index.php");
+include_once "layout.php";
 ?>
 
 
@@ -8,52 +9,18 @@ include_once("../Models/index.php");
 <html>
 <head>
   <title>Crear un nuevo post</title>
-  <meta charset="UTF-8">
-  <title>Crear Post</title>
-  <script src="https://kit.fontawesome.com/41bcea2ae3.js" crossorigin="anonymous"></script>
-  <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet"
-    integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
-    <link rel="stylesheet" href="http://localhost/Proyecto_Lenguajes/Views/Assets/css/post.css">
-  <link rel="stylesheet" href="http://localhost/Proyecto_Lenguajes/Views/Assets/css/servicios.css">
-
+  <link rel="stylesheet" href="http://localhost/Proyecto_Lenguajes/Views/Assets/css/post.css">
+  <?php MostrarHead(); ?>
 </head>
 <body>
-  <nav>
-    <div class="nav-container">
-      <div class="main-info-container">
-        <img class="logo-img" src="../Views/Assets/imgs/logo.png" alt="" />
-      </div>
-      <div class="data-container">
-        <div class="servicios">
-          <a class="nav-link Dropdown" aria-current="page" href="../Views/servicios.html">
-            <h5>SERVICIOS</h5>
-          </a>
-        </div>
-        <div class="servicios">
-          <a class="nav-link Dropdown" aria-current="page" href="../Views/home.html">
-          <h5>PAQUETES DE VIAJE</h5>
-        </a>
-        </div>
-        <div class="servicios">
-          <a class="nav-link Dropdown" aria-current="page" href="../Views/blog.html">
-            <h5>BLOG</h5>
-          </a>
-        </div>
-        <div class="servicios">   
-          <a class="nav-link Dropdown" aria-current="page" href="../Views/Buscador.html">
-          <h5>BUSCADOR VUELOS</h5>
-        </a>
-        </div>
-      </div>
-    </div>
-  </nav>
+<?php 
+        MostrarHeader();
+    ?>
   <div class="admin-wrapper">
     <!-- izquierda-->
     <div class="left-sidebar">
         <ul>
-            <li><a href="../Views/blog.html">Ver Blog</a></li>
+            <li><a href="../Views/blog.php">Ver Blog</a></li>
             <li><a href="../Views/adminblog.php">Administrar Post</a></li>
         </ul>
     </div>
