69b6995b65fb8c4b193032080966b5ef29cfffb1
Angel Salmeron <shangel2003@gmail.com>
2023-08-21 18:37:38 -0600

    Funcion Editar

--- Views/prueba.php
+++ Views/prueba.php
@@ -1,4 +1,5 @@
 <?php
+
 if (isset($_POST['btnpublicar']) && $_POST['btnpublicar'] === 'Guardar') {
     include_once("../Controllers/controlblog.php");
     modeloControllerblog::guardar();
@@ -15,5 +16,27 @@ if (isset($_GET['m']) && $_GET['m'] === 'eliminar' && isset($_GET['id'])) {
     } else {
         echo($pblog);
     }
+}
+if (isset($_POST['m']) && $_POST['m'] === 'actualizar') {
+    include_once("../Controllers/controlblog.php");
+    $id = $_POST['id'];
+    $titulo = $_POST['titulo'];
+    $mensaje = $_POST['mensaje'];
+    $destino = $_POST['Destino'];
+
+    $pblog = new modeloControllerblog();
+    
+    try {
+        $pblog->actualizarPost($id, $titulo, $mensaje, $destino);
+        if ($pblog) {
+            header("Location: http://localhost/proyecto_Lenguajes/Views/adminblog.php");
+            exit(); 
+        } else {
+            echo($exito);
+        }
+    } catch (Exception $e) {
+        echo "Error al actualizar los datos: " . $e->getMessage();
+    }
+    
 }
 ?>
